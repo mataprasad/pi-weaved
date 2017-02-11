098741d30aa110706dcf198e086232754192c3a2
mataprasad045 <mataprasad045@gmail.com>
2017-02-11 19:04:11 +0530

    remove bundel

--- CaptureMyCurrentIp/Global.asax.cs
+++ CaptureMyCurrentIp/Global.asax.cs
@@ -21,7 +21,6 @@ namespace CaptureMyCurrentIp
             WebApiConfig.Register(GlobalConfiguration.Configuration);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
         }
     }
 }
